5f7296a516d989355f41cb4d43f74f24f9b89b8e
super <super@curstdd.com>
2018-07-06 12:01:58 +0200

    creacion test para Gestio

---
+++ src/test/java/gestio/GestioTest.java
@@ -0,0 +1,14 @@
+package gestio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestioTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
